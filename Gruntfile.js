e14101d5bcf0fa11992f2528dee8701884509bf3
Stuart Colville <scolville@mozilla.com>
2015-06-02 10:52:25 +0100

    Remove releases builder and replace with direct npm deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,21 +11,5 @@ module.exports = function(grunt) {
   });
 
   grunt.initConfig(configs);
-
-  grunt.registerTask('publish-release', function() {
-    // Require the build
-    this.requires(['build']);
-
-    if (process.env.TRAVIS === 'true' &&
-        process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
-        process.env.TRAVIS_PULL_REQUEST === 'false') {
-      grunt.log.writeln('Pushing branch for release');
-      grunt.task.run('gh-pages:release');
-    } else {
-      grunt.log.writeln('Skipped release publication');
-    }
-  });
-
-  grunt.registerTask('build', ['webpack:dist']);
   grunt.registerTask('test', ['webpack:dist', 'karma:run', 'eslint']);
 };
--- tests/test.gh-pages-config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ghPagesConfig = require('../tasks/gh-pages');
-
-describe('grunt-gh-pages config', function() {
-  it('Relase branch publication should have silent=true', function() {
-    assert.equal(ghPagesConfig.release.options.silent, true);
-  });
-});
