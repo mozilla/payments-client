e226983680c57e49a3f2e5fa35e3b9acdfbdf1aa
Stuart Colville <scolville@mozilla.com>
2015-07-13 11:42:21 +0100

    Rename file karma.config.shared.js -> karma.shared.js

--- karma.conf.sauce.js
+++ karma.conf.sauce.js
@@ -4,7 +4,7 @@
 'use strict';
 
 var defaults = require('lodash.defaults');
-var karmaConfig = require('./karma.config.shared');
+var karmaConfig = require('./karma.shared');
 var browsers = require('mozilla-payments-saucelabs-browsers');
 
 
